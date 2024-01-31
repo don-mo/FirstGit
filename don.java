3e70d6cf32f31ee970c00984ae3793d5bc904920
Don <donmo99@gmail.com>
2024-01-31 11:47:46 -0800

    Added new line to don.java

--- don.java
+++ don.java
@@ -2,5 +2,6 @@ public class don {
   public static void main(String[] args) {
     System.out.println("Bruh");
     System.out.println("Hello world");
+    System.out.println("Bruhhhhhhhh 2");
   }
 }
